59f935358d89140dadb2eefbe2d8b1d72c2e26ed
vandeurenglenn <vandeurenglenn@gmail.com>
2022-03-12 16:30:30 +0100

    Add networkName & networkVersion options

--- src/client.js
+++ src/client.js
@@ -17,10 +17,11 @@ globalThis.pubsub = globalThis.pubsub || new Pubsub({verbose: false})
 export default class PeernetClient {
   constructor(options = {}) {
     if (!options.id) options.id = Buffer.from('00000000000000000000000000000000')
-
+    if (!options.networkVersion) options.networkVersion = 'v0.1.0'
+    if (!options.networkName) options.networkName = 'peernet'
     this.id = options.id
 
-    this.topic = Buffer.from(sha256('peernet-v0.1.0').toString())
+    this.topic = Buffer.from(sha256(`${option.networkName}-${options.networkVersion}`).toString())
 
     const trackers = [
       'wss://star.leofcoin.org:7575',
@@ -42,71 +43,6 @@ export default class PeernetClient {
 
     this.p2p.start()
 
-
-    // this.sw.join(this.topic.slice(0, 32))
-    //
-    // const arrayOfPeers = this.sw.getPeers()
-    //
-    // this.sw.on('connection', async (connection, info) => {
-    //   // connected
-    //   const id = info.id.toString()
-    //   if (id === this.id.toString()) return
-    //
-    //   const channel = connection.channelName
-    //   let channels = [channel]
-    //   let peer
-    //   if (connections.has(id)) {
-    //     const value = connections.get(id)
-    //     const _channels = value.channels
-    //     channels = [...channels, ..._channels]
-    //     peer = value.peer
-    //   } else {
-    //     peer = new PeernetPeer(id, connection)
-    //     pubsub.publish('peer:connected', peer)
-    //   }
-    //   connections.set(id, {channels, peer})
-    //   // connection.on('data', (data) => console.log({data}))
-    //   // connection.write('peerId')
-    // });
-    //
-    // this.sw.on('candidates-updated', async (channel, candidates) => {
-    //   // TODO: Channels ...
-    //   for (const candidate of candidates) {
-    //     const id = candidate.toString()
-    //     let channels = [channel]
-    //     if (id === this.id.toString()) return
-    //
-    //     if (!connections.has(id)) {
-    //       try {
-    //         const connection = await this.sw.connect(channel, candidate)
-    //         const peer = new PeernetPeer(id, connection)
-    //         connections.set(id, {channels, peer})
-    //         if (recentConnections.has(id)) {
-    //           setTimeout(() => {
-    //             pubsub.publish('peer:connected', peer)
-    //           }, 1000)
-    //         } else {
-    //           pubsub.publish('peer:connected', peer)
-    //         }
-    //       } catch (e) {
-    //         console.warn(e);
-    //       }
-    //     } else {
-    //       const value = connections.get(id)
-    //       const _channels = value.channels
-    //       channels = [...channels, ..._channels]
-    //       connections.set(id, {channels, peer: value.peer})
-    //     }
-    //   }
-    // })
-    //
-    // this.sw.on('connection-closed', (connection, info) => {
-    //   if (!recentConnections.has(info.id.toString())) {
-    //     recentConnections.set(info.id.toString(), new Date().getTime())
-    //   }
-    //   if (connections.has(info.id.toString())) connections.delete(info.id.toString())
-    // })
-    //
     if (globalThis.process) {
       process.on('SIGINT', async () => {
         console.log('Caught interrupt signal')
