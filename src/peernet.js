1a9390e7965b5e1263bf5b0de6bc030aea6cd426
vandeurenglenn <vandeurenglenn@gmail.com>
2022-05-08 16:03:02 +0200

    Fix browser build

--- src/peernet.js
+++ src/peernet.js
@@ -86,7 +86,7 @@ export default class Peernet {
     if (this.hasDaemon) {
       Storage = LeofcoinStorageClient
     } else {
-      Storage = globalThis.LeofcoinStorage?.default ? globalThis.LeofcoinStorage.default : LeofcoinStorage
+      Storage = globalThis.LeofcoinStorage ? globalThis.LeofcoinStorage : LeofcoinStorage
     }
     globalThis[`${name}Store`] = globalThis[`${name}Store`] ||
       await new Storage(name, root)
