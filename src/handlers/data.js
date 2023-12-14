b39b1a4749f5a4d7a9c0dd8ad4e35ebd1b9977a1
VandeurenGlenn <vandeurenglenn@gmail.com>
2023-12-14 13:56:27 +0100

    Don't just throw error if proto not found, log it

--- src/handlers/data.js
+++ src/handlers/data.js
@@ -3,9 +3,13 @@ import { protoFor } from './../utils/utils.js'
 const dataHandler = async (message) => {
   if (!message) return
 
-  const { data, id, from, peer } = message
-  const proto = await protoFor(data)
-  peernet._protoHandler({ id, proto }, peernet.connections[from] || peer, from)
+  try {
+    const { data, id, from, peer } = message
+    const proto = await protoFor(data)
+    peernet._protoHandler({ id, proto }, peernet.connections[from] || peer, from)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export default dataHandler
